6349b4ef1130ca3112b46323fee8e258d4d0f4be
GoodWorld <Kawinphop.jun@gmail.com>
2025-03-14 01:33:35 +0700

    Update config.h

--- PowerSensorModule/config.h
+++ PowerSensorModule/config.h
@@ -1,6 +1,6 @@
-#define WIFI_SSID    "KUWIN-IOT"
+#define WIFI_SSID    ""
 #define WIFI_PASS    ""
-#define MQTT_BROKER  "iot.cpe.ku.ac.th"
-#define MQTT_USER    "b6710503739"  // ไม่ต้องใส่เครื่องหมาย <>
-#define MQTT_PASS    "kawinphop.j@ku.th"      // ไม่ต้องใส่เครื่องหมาย <>
-#define TOPIC_PREFIX "b6710503739"  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_BROKER  ""
+#define MQTT_USER    ""  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_PASS    ""      // ไม่ต้องใส่เครื่องหมาย <>
+#define TOPIC_PREFIX ""  // ไม่ต้องใส่เครื่องหมาย <>
